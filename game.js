0085a5194c74753ac208fd5f0796bacd4cc3e669
Gyorgy Papp <43781424+gurgeasson@users.noreply.github.com>
2022-11-08 13:57:00 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
-const element = document.getElementById("Rock");
-element.addEventListener("click", myFunction);
+const elementRock = document.getElementById("Rock");
+const elementPaper = document.getElementById("Paper");
+const elementSciss = document.getElementById("Scissors");
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Smash";
+elementRock.addEventListener("click", myFunction(R));
+elementPaper.addEventListener("click", myFunctionP);
+elementSciss.addEventListener("click", myFunctionS);
+
+      function myFunctionR() {
+        document.getElementById("demo").innerHTML = "Smash";
+      }
+
+      function myFunctionP() {
+        document.getElementById("demo").innerHTML = "Rustle";
+      }
+
+      function myFunctionS() {
+        document.getElementById("demo").innerHTML = "Snip-Snip";
+      }
+
+
+/*set a variable to message (smash or rustle or snip-snip)
+
+function RPS(hand it down message variable) {
+  document.getElementByID("demo").innerHTML = "message variable";
+*/
+
+
 }
